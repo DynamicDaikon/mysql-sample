f2b51a6f492fc2bb57e70119518afefc8193d002
DynamicDaikon <hietasoba@gmail.com>
2025-01-26 22:25:45 +0900

    [fix]add docker-compose.yml

--- docker/db/sql/sql001.sql
+++ docker/db/sql/sql001.sql
@@ -1,10 +1,15 @@
-CREATE TABLE countries (
+DROP TABLE IF EXISTS `countries`;
+
+CREATE TABLE IF NOT EXISTS `countries` (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50),
     ranking INT,
     group_name VARCHAR(1)
-)
-CREATE TABLE players (
+);
+
+DROP TABLE IF EXITSTS `players`;
+
+CREATE TABLE IF NOT EXISTS `players` (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     country_id INT,
     uniform_num INT,
@@ -14,17 +19,23 @@ CREATE TABLE players (
     birth DATE,
     height INT,
     weight INT FOREIGN KEY (country_id) REFERENCES countries (id) ON DELETE CASCADE
-)
-CREATE TABLE pairings (
+);
+
+DROP TABLE IF EXISTS `pairings`;
+
+CREATE TABLE IF NOT EXISTS `pairings` (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     kickoff datetime,
     my_country_id INT,
     enemy_country_id INT FOREIGN KEY (my_country_id, enemy_country_id) REFERENCES countries (id, id) ON DELETE CASCADE,
-)
-CREATE TABLE goals (
+);
+
+DROP TABLE IF EXISTS `goals`;
+
+CREATE TABLE IF NOT EXISTS `goals` (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     pariring_id INT,
     player_id INT,
     goal_time VARCHAR(10) FOREIGN KEY (pairing_id) REFERENCES pairings (id) ON DELETE CASCADE,
     FOREIGN KEY (player_id) REFERENCES players (id) ON DELETE CASCADE
-)
+);
